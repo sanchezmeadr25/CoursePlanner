df15c489666f10a7fd57c1fa92ebacebfcf5b3fe
Adrián Sánchez Méndez <sanchez.meadr25@triana.salesianos.edu>
2026-05-19 12:30:23 +0200

    Consulta JPA

--- CourserPlanner/src/main/java/com/salesianostriana/dam/courserplanner/repositorio/EstudianteRepositorio.java
+++ CourserPlanner/src/main/java/com/salesianostriana/dam/courserplanner/repositorio/EstudianteRepositorio.java
@@ -1,10 +1,19 @@
 package com.salesianostriana.dam.courserplanner.repositorio;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.salesianostriana.dam.courserplanner.modelo.Estudiante;
+import com.salesianostriana.dam.courserplanner.modelo.Inscripcion;
 
-
+@Repository
 public interface EstudianteRepositorio extends JpaRepository<Estudiante, String>{
 
+	public List<Estudiante> findByInscripcion(Inscripcion inscipcion); 
+	
+	public Estudiante findByNombre(String nombre);
+	
+	
 }
