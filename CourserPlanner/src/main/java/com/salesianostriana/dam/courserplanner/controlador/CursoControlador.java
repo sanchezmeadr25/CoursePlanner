56850d24b65d8a72abce1d95e159fe0676e25f17
sanchezmeadr25-prog <sanchez.meadr25@triana.salesianos.edu>
2026-05-26 19:10:26 +0200

    creado el listar Cursos

--- CourserPlanner/src/main/java/com/salesianostriana/dam/courserplanner/controlador/CursoControlador.java
+++ CourserPlanner/src/main/java/com/salesianostriana/dam/courserplanner/controlador/CursoControlador.java
@@ -1,5 +1,6 @@
 package com.salesianostriana.dam.courserplanner.controlador;
 
+import java.security.Principal;
 import java.util.Optional;
 
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -13,6 +14,7 @@ import com.salesianostriana.dam.courserplanner.modelo.Curso;
 import com.salesianostriana.dam.courserplanner.modelo.Instructor;
 import com.salesianostriana.dam.courserplanner.modelo.Usuario;
 import com.salesianostriana.dam.courserplanner.repositorio.InstructorRepositorio;
+import com.salesianostriana.dam.courserplanner.repositorio.UsuarioRepositorio;
 import com.salesianostriana.dam.courserplanner.servicio.CursoServicio;
 import com.salesianostriana.dam.courserplanner.servicio.InstructorServicio;
 
@@ -22,13 +24,14 @@ public class CursoControlador {
 	private final CursoServicio cursoServicio;
 	private final InstructorRepositorio instructorRepositorio;
 	private final InstructorServicio instructorServicio;
+	private final UsuarioRepositorio usuarioRepositorio;
 	
-	
-	public CursoControlador(CursoServicio cursoServicio, InstructorRepositorio instructorRepositorio,InstructorServicio instructorServicio) {
+	public CursoControlador(CursoServicio cursoServicio, InstructorRepositorio instructorRepositorio,InstructorServicio instructorServicio,UsuarioRepositorio usuarioRepositorio) {
 		super();
 		this.cursoServicio = cursoServicio;
 		this.instructorRepositorio = instructorRepositorio;
 		this.instructorServicio = instructorServicio;
+		this.usuarioRepositorio = usuarioRepositorio;
 	}
 	
 	
@@ -45,14 +48,21 @@ public class CursoControlador {
 		return "redirect:/";
 	}
 	
-	@GetMapping("/misCursos")
-	public String listarMisCursos(Model model,@AuthenticationPrincipal Usuario usuario) {
-		Optional<Instructor> instructor = instructorServicio.buscarPorId(usuario.getDni());
-				
-		model.addAttribute("listaCurso",cursoServicio.findCursosByInstructor(instructor));
-		
-		return "admin/listaCursos";
-	}
+	@GetMapping("admin/misCursos")
+	public String listarMisCursos(Model model, Principal principal) {
+	    
+		String username = principal.getName();
+	    
+	    // 1. Buscamos el Optional
+	    Optional<Usuario> usuarioOpt = usuarioRepositorio.findByUsername(username);
+	    
+	    Usuario usuario = usuarioOpt.get();
+
+	    model.addAttribute("listaCurso", cursoServicio.findCursosByInstructor(usuario.getDni()));
+	    
+	    return "admin/listaCursos";
+	    	
+	    }
 	
 	
 	
